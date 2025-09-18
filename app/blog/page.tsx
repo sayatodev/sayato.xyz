18d92fdc7ffb1e7642d6da3ef9982497409d5153
Sayato <amf.kano@gmail.com>
2025-09-18 22:46:25 +0800

    fix(blog): Fix responsiveness issues for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,23 +1,17 @@
-import { SectionContainer } from "@/app/_components/SectionContainer";
-
 import styles from "@/app/blog/page.module.css";
 
 export default function Home() {
   return (
-    <>
-      <SectionContainer mono>
-        <div className={styles.blogListContainer}>
-          <pre className={styles.hintText}>
-            ┌──(sayato@procrastination101)-[~]
-            <br />
-            └─$ cat blog-list.txt
-            <br />
-            cat: blog-list.txt: No such file or directory
-            <br />
-            ...Stay tuned for future updates!
-          </pre>
-        </div>
-      </SectionContainer>
-    </>
+    <div className={styles.blogListContainer}>
+      <pre className={styles.hintText}>
+        ┌──(sayato@procrastination101)-[~]
+        <br />
+        └─$ cat blog-list.txt
+        <br />
+        cat: blog-list.txt: No such file or directory
+        <br />
+        ...Stay tuned for future updates!
+      </pre>
+    </div>
   );
 }
